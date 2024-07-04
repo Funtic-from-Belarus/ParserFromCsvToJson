d18e1962d9c6fe94b68aa8df2497057ebad20a88
Funtic-from-Belarus <maksim.kuzyuk@gmail.com>
2024-07-04 12:49:54 +0300

    exceptions
    check are there names of columns with audiofile and transcript, more than 2 columns with both columns name audiofile and transcript and trycatch to read_csv

--- parser.py
+++ parser.py
@@ -25,7 +25,7 @@ def create_json_data(df):
                             "to_name": "audio",
                             "type": "textarea",
                             "value": {
-                                "text": row['transcript']
+                                "text": row["transcript"]
                             }
                         }
                     ]
@@ -40,26 +40,42 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--in", dest="input_file",
                         required=True)
-    parser.add_argument("--out", dest="output_csv",
-                        required=True)
     parser.add_argument("--to", dest="output_json",
                         required=True)
     args = parser.parse_args()
 
-    df = pd.read_csv(args.input_file, sep='\t', header=None, names=['data'])
+    try:
+        df = pd.read_csv(args.input_file)
+    except:
+        print("\nВнимание: колонки в файле некорректны")
+        exit(1)
 
-    data = df['data'].str.extract(r'(.*?)pri_(\d\d)(.*)', expand=True)
-    data[0] = data[0].str.strip()
-    data[1] = 'pri_' + data[1].str.strip()
-    data[2] = data[2].str.strip()
+    if len(df.columns) == 2 and ('audiofile' in df.columns or 'transcript' in df.columns):
+        result = df[df['transcript'].notna()]
+        json_data = create_json_data(result)
+        with open(args.output_json, 'w', encoding='utf-8') as f:
+            json.dump(json_data, f, indent=4, ensure_ascii=False)
+        exit(1)
+    elif len(df.columns) == 2 and not ('audiofile' in df.columns or 'transcript' in df.columns):
+        df.columns = ['audiofile', 'transcript']
+        print("Внимание: колонки в файле должны быть названы 'audiofile' и 'transcript'.")
+    elif 'audiofile' in df.columns and 'transcript' in df.columns and len(df.columns) > 2:
+        df = df[['audiofile', 'transcript']]
+    else:
+        print("Внимание: колонки в файле должны быть названы 'audiofile' и 'transcript'.")
+        exit(1)
 
-    result = pd.DataFrame(
-        {'audiofile': data[0] + data[1], 'transcript': data[2]})
-    result = result[result['transcript'].notna()]
-
-    result.to_csv(args.output_csv, index=False)
-
+    result = df[df['transcript'].notna()]
     json_data = create_json_data(result)
+    with open(args.output_json, 'w', encoding='utf-8') as f:
+        json.dump(json_data, f, indent=4, ensure_ascii=False)
 
-    with open(args.output_json, 'w') as f:
-        json.dump(json_data, f, indent=4)
+    # data = df['data'].str.extract(r'(.*?)pri_(\d\d),(.*)', expand=True)
+    # data[0] = data[0].str.strip()
+    # data[1] = 'pri_' + data[1].str.strip()
+    # data[2] = data[2].str.strip()
+
+    # result = pd.DataFrame(
+    #     {'audiofile': data[0] + data[1], 'transcript': data[2]})
+
+    # result.to_csv(args.output_csv, index=False)
